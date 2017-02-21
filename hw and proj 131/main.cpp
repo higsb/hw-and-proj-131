3fa2398c78403eb5142e238b02f5c8b65934c801
Dennis Durant II <dennisdurant2@denniss-mbp.fios-router.home>
2017-02-20 19:56:06 -0800

    Initial Commit

---
+++ hw and proj 131/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw and proj 131
+//
+//  Created by Dennis Durant II on 2/20/17.
+//  Copyright © 2017 Dennis Durant II. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
